eef6b137a0a710bfdd65dc3a7f9ea7b6e6547ada
Elisa Bourgeois <elisabourgeois.eb@gmail.com>
2018-11-20 21:58:25 +0100

    Creation of the Home page and integration of a BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import Home from "./components/Home";
+
+//switch to typography v2 in material ui
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Route exact path="/" component={Home} />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
